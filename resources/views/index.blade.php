f3fc607baaa5fb71478af357fdff022621cd3eb2
Rinderuu <lhyndelsario94@gmail.com>
2024-10-31 05:31:43 +0800

    hays

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -130,40 +130,7 @@
     </style>
 </head>
 <body>
-<<<<<<< HEAD
-<!-- Navigation Bar -->
-<nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">ETC</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="calculate">Calculate</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact">Contact Us</a>
-                </li>
-            </ul>
-            <div class="d-flex">
-                <a href="loginpage"><button class="btn btn-outline-primary me-2" type="button">Login</button></a>
-                <a href="signup"><button class="btn btn-primary-signup" type="button">Sign Up</button></a>
-            </div>
-        </div>
-    </div>
-</nav>
-=======
 @include('partials.navbarguest')
->>>>>>> ed07bad (first commit)
 
 
 <!-- Hero Section -->
